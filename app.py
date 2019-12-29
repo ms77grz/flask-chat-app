6c9e9c2aa8ec7e256bae53368819bbc9d657d176
ms77grz <ms77grz@gmail.com>
2019-12-29 23:01:55 +0300

    part-4

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, join_room, leave_room
-from db import get_user
+from db import get_user, save_user
+from pymongo.errors import DuplicateKeyError
 
 app = Flask(__name__)
 app.secret_key = 'my simple secret key'
@@ -34,6 +35,23 @@ def login():
         else:
             message = 'Failed to login!'
     return render_template('login.html', message=message)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    message = ''
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        try:
+            save_user(username, email, password)
+            return redirect(url_for('login'))
+        except DuplicateKeyError:
+            message = 'User already exists!'
+    return render_template('signup.html', message=message)
 
 
 @app.route('/logout')
